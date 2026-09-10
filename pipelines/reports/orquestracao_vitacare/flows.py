a6db2eb84333ab72e263b4e599885c5f44de5886
Herian <herian.cavalcante@live.com>
2026-09-10 16:21:41 -0300

    fix: update sqlserver_backup parameters to use updated environment parameters

--- pipelines/reports/orquestracao_vitacare/flows.py
+++ pipelines/reports/orquestracao_vitacare/flows.py
@@ -43,7 +43,7 @@ def orquestracao_vitacare(
   sqlserver_backup_params.update(environment_params)
   fr_sqlserver = create_flow_run(
     flow=sqlserver_backup,
-    parameters=constants.SQLSERVER_BACKUP_PARAMS.value,
+    parameters=sqlserver_backup_params,
     environment=environment,
   )
   wait_for_flow_run_task(flow_run_id=fr_sqlserver.id)
