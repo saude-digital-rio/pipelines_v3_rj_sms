6cab5fb19e4012e3d79035095f4bad15a47cd703
Herian <herian.cavalcante@live.com>
2026-09-14 15:14:42 -0300

    feat (sqlserver): adiciona logging auxiliar

--- pipelines/datalake/migrate/sqlserver_backup/flows.py
+++ pipelines/datalake/migrate/sqlserver_backup/flows.py
@@ -55,8 +55,9 @@ def sqlserver_backup(
 
     instance_started = True
     start_instance(instance_name=instance_name)
-
-    for restore_item in restore_plan:
+    restore_plan_len = len(restore_plan)
+    for i, restore_item in enumerate(restore_plan):
+      log(f"Backup {i}/{restore_plan_len}")
       result = restore_backup(restore_item=restore_item, instance_name=instance_name)
       results.append(result)
 
