a0128324eefef0126cc52e4d87635c4d3e10bfe2
Matheus Avellar <matheus.avellar@prefeitura.rio>
2026-09-15 13:50:12 -0300

    style: lint

--- pipelines/datalake/migrate/clone_bigquery/schedules.py
+++ pipelines/datalake/migrate/clone_bigquery/schedules.py
@@ -68,12 +68,10 @@ daily_flow_parameters = [
   {
     "source_project_name": "iplan-rio-sms",
     "source_dataset_name": "billing",
-    "source_table_list": [
-      "gcp_billing_export_resource_v1_01BAED_6E64B4_606041"
-    ],
+    "source_table_list": ["gcp_billing_export_resource_v1_01BAED_6E64B4_606041"],
     "destination_dataset_name": "brutos_faturamento_nuvem_staging",
     "environment": "prod",
-  }
+  },
 ]
 
 weekly_flow_parameters = [
