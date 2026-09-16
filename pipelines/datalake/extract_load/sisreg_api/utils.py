4bc8bc5cfbc03d39e7a55255d80d96161e87ed34
Matheus Avellar <matheus.avellar@prefeitura.rio>
2026-09-16 14:51:15 -0300

    chore: print de arquivos e tamanhos

--- pipelines/datalake/extract_load/sisreg_api/utils.py
+++ pipelines/datalake/extract_load/sisreg_api/utils.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 from datetime import date, datetime, timedelta
+import os
+from pathlib import Path
 from typing import Literal, Optional, Tuple
 
 from elasticsearch import Elasticsearch, exceptions
 from prefect import State, Task
 
-from pipelines.utils.cleanup import cleanup_bigquery_name
+from pipelines.utils.cleanup import cleanup_bigquery_name, prettify_byte_size
 from pipelines.utils.datetime import parse_date_or_today
 from pipelines.utils.logger import log
 from pipelines.utils.monitor import get_ram_snapshot
@@ -128,4 +130,14 @@ def handle_task_state_change(task: Task, task_run, state: State):
     f"{snapshot['total_pretty']} "
     f"({snapshot['used_pct']:.2f}%)"
     "\n====================="
+  )
+
+  files = []
+  for path in Path("/tmp/pipelines").rglob('*'):
+    if path.is_file():
+      files.append(f"{path}: {prettify_byte_size(path.stat().st_size)}")
+
+  log(
+    "Arquivos em /tmp:\n"
+    + "\n".join(sorted(files))
   )
