2f3542eb575424e861c475062a41ea83fec50103
Herian <herian.cavalcante@live.com>
2026-09-25 14:01:15 -0300

    remove: remove every_4_hours schedule from dbt flow

--- pipelines/datalake/transform/dbt/schedules.py
+++ pipelines/datalake/transform/dbt/schedules.py
@@ -54,25 +54,9 @@ monthly_schedule = [
   )
 ]
 
-every_4_hours_schedule = [
-  Interval(
-    timedelta(hours=4),
-    anchor_date=datetime(2026, 1, 1, 9, 0, tzinfo=constants.TIMEZONE.value),
-    timezone=constants.TIMEZONE_NAME.value,
-    parameters={
-      "command": "build",
-      "environment": "prod",
-      "rename_flow": True,
-      "select": "tag:cdi-4hours",
-      "send_discord_report": False,
-    },
-  )
-]
-
 
 schedules = [
   *daily_schedule,
   *weekly_schedule,
   *monthly_schedule,
-  *every_4_hours_schedule,
 ]
